4b583c6a68e805fb8558c7f871a28e4ae1af9cf2
SamiAibeche <samiaibeche@gmail.com>
2018-12-19 16:21:21 +0100

    Projects - Comments

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -42,12 +42,19 @@ class ProjectController extends AbstractController
      */
     public function add(Request $request, ValidatorInterface $validator)
     {
+        //Init vars
         $project = new Project();
         $form = $this->createForm(ProjectType::class, $project);
+
+        //Get current request
         $form->handleRequest($request);
 
+        //On submit
         if($form->isSubmitted()){
+
+            //Check if there are some errors
             $errors = $validator->validate($project);
+            //If yes, show error message
             if (count($errors) > 0) {
                 return $this->render('admin/project/add.html.twig', [
                     'form'              => $form->createView(),
@@ -55,11 +62,12 @@ class ProjectController extends AbstractController
                     'errors' => $errors
                 ]);
             }
-
+            //If not, store the data
             if($form->isValid()) {
 
                 if($form->get('image')->getData() !== true) {
 
+                    //Save file if it has been posted
                     if (!is_null($form->get('image')->getData())) {
                         // Get Posted Image datas
                         $file = $form->get('image')->getData();
@@ -73,15 +81,15 @@ class ProjectController extends AbstractController
                             );
                         } catch (FileException $e) {
                             // ... handle exception if something happens during file upload
-                        }
+                            throw new FileException("Something's wrong with the file upload.");
 
+                        }
                         // instead of its contents
                         $project->setImage($fileName);
                     }
                 }
 
-
-                // get data in form
+                // Get data in form
                 $this->getDoctrine()->getManager()->persist($project);
                 $this->getDoctrine()->getManager()->flush();
                 $this->get('session')->getFlashBag()->set('success', 'The Project has been successfully created !');
@@ -89,7 +97,7 @@ class ProjectController extends AbstractController
             }
         }
 
-
+        //Render
         return $this->render('admin/project/add.html.twig', [
             'form'              => $form->createView(),
             'controller_name'   => 'ProjectController'
@@ -101,16 +109,21 @@ class ProjectController extends AbstractController
      */
     public function edit(Request $request, ValidatorInterface $validator, $id)
     {
+        //Init vars
         $repo = $this->getDoctrine()->getRepository(Project::class);
         $project = $repo->find($id);
         $olderFile = $project->getImage();
         $form = $this->createForm(ProjectType::class, $project);
+
+        //Get current request
         $form->handleRequest($request);
 
+        //On submit
         if($form->isSubmitted()) {
 
-
+            //Check if there are some errors
             $errors = $validator->validate($project);
+            //If yes, show error message
             if (count($errors) > 0) {
                 return $this->render('admin/project/add.html.twig', [
                     'form'              => $form->createView(),
@@ -118,9 +131,12 @@ class ProjectController extends AbstractController
                     'errors' => $errors
                 ]);
             }
-
+            //If not, store the data
             if($form->isValid()){
+
+                //Save file if it has been posted
                 if($form->get('image')->getData() !== true) {
+
                     //Remove previous file
                     $oldImagePath = $this->getParameter('projects_directory') . "/" . $olderFile;
                     if (file_exists($oldImagePath)) {
@@ -139,15 +155,17 @@ class ProjectController extends AbstractController
                         );
                     } catch (FileException $e) {
                         // ... handle exception if something happens during file upload
+                        throw new FileException("Something's wrong with the file upload.");
                     }
 
                     // instead of its contents
                     $project->setImage($fileName);
                 } else {
+                    //Keep previous filename if it has not been posted
                     $project->setImage($olderFile);
                 }
 
-                // get data in form
+                //Get data in form
                 $this->getDoctrine()->getManager()->persist($project);
                 $this->getDoctrine()->getManager()->flush();
                 $this->get('session')->getFlashBag()->set('success', 'The Project has been successfully updated');
@@ -158,10 +176,7 @@ class ProjectController extends AbstractController
                 ]);
             }
         }
-
-
-
-
+        //Render
         return $this->render('admin/project/add.html.twig', [
             'form'            => $form->createView(),
             'controller_name' => 'ProjectController',
